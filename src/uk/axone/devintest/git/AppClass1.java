b0c87ba7b23ccccd32b5af8ac3faa562f4a549e0
Kavitha <kavsmisc@gmail.com>
2021-02-28 08:23:31 +0000

    Jira111 - adding a practice method to pulled repo

--- src/uk/axone/devintest/git/AppClass1.java
+++ src/uk/axone/devintest/git/AppClass1.java
@@ -2,11 +2,15 @@ package uk.axone.devintest.git;
 
 public class AppClass1 {
 
-    public void doSomething(){
+    public void doSomething() {
         //testing git commit
         System.out.println("do something");
-
-        //adding comment
     }
+        //adding comment
 
+        //adding a practice github method
+        public void practiceMethod(){
+            System.out.println("adding second method");
+
+    }
 }
